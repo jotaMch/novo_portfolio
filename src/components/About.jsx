b3bf21409b0dd377814d7237c35560bdc9ef87f1
Jânderson Machado <jandersonmachado090@gmail.com>
2023-09-04 13:00:05 -0300

    add new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ function About() {
                                         contate-me
                                     </button>
                                     <button class="button__curriculo">
-                                        <a href={CvDocx} >
+                                        <a>
                                             <div class="text">Currículo< BiAlignLeft /></div>
                                             <span class="icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"></path></svg>
